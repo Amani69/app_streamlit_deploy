bed2d3a01a68ba11cfb81debd6c467ce640d7b8f
amani <ellafi.amani@gmail.com>
2023-01-18 23:36:46 +0100

    redme

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# import
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -18,23 +17,16 @@ import streamlit_authenticator as stauth
 from streamlit_option_menu import  option_menu 
 from streamlit_lottie import st_lottie
 import json
-import requests  # pip install requests
+import requests  
 import sklearn
 
 
-
-#lottie
 def load_lottiefile(filepath: str):
     with open(filepath, "r") as f:
         return json.load(f)
 
-
-
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Community Attrition Management", page_icon="::chart_with_upwards_trend:", layout="wide")
 
-
-# user authentication
 
 users=db.fetch_all_users() 
 usernames=[user["key"] for user in users]
@@ -44,7 +36,7 @@ hashed_passwords=[user["password"] for user in users]
 authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
     "Eclaireurs_prediction", "abcdef", cookie_expiry_days=30)
 
-lottie_log = load_lottiefile("login.json")  # replace link to local lottie file
+lottie_log = load_lottiefile("login.json") 
 lottie_go = load_lottiefile("go.json") 
 
 
@@ -61,8 +53,7 @@ if authentication_status == None:
     speed=1,
     reverse=True,
     loop=True,
-    quality="high", # medium ; high
-    #renderer="canvas", # canvas,svg
+    quality="high", 
     height=300,
     width=300,
     key=None,
@@ -71,16 +62,9 @@ if authentication_status == None:
 
 if authentication_status:
      
-    
-#ajouter le titre 
 
-    # Add a sidebar to the web page. 
-   # st.markdown('---')
-    
     authenticator.logout("Logout", "sidebar")
     st.sidebar.title(f"Welcome {name}")
-    # Sidebar Configuration
-    #st.sidebar.image('https://is4-ssl.mzstatic.com/image/thumb/Purple125/v4/10/ba/d3/10bad386-6c5f-cf95-c227-6690d321a298/AppIcon-0-0-1x_U007emarketing-0-0-0-5-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/256x256bb.jpg', width=200)
     st.sidebar.image('Capture14.png', width=100)
    
     st.sidebar.markdown('# Eclaireurs')
@@ -94,9 +78,8 @@ if authentication_status:
                 Ces données seront traitées et  valorisées surtout de par leur exploitation.                         """)
 
 
-    # Graph (Bar Chart in Sidebar)
+   
     df2= pd.read_csv('data_date.csv')
-    # Plot 
     category_tr = df2.groupby(by='year')['token'].count().to_frame().reset_index()
 
     fig1 = px.bar(category_tr, x='year', y='token',
@@ -110,16 +93,6 @@ if authentication_status:
                    )
     st.sidebar.plotly_chart(fig1, use_container_width=True)
 
-# #pour enlever made in streamlit, parmas , ligne...
-#    # --- HIDE STREAMLIT STYLE ---
-#     hide_st_style = """
-#                 <style>
-#                 #MainMenu {visibility: hidden;}
-#                 footer {visibility: hidden;}
-#                 header {visibility: hidden;}
-#                 </style>
-#                 """
-#     st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 
@@ -129,27 +102,25 @@ if authentication_status:
         st.title(":chart_with_upwards_trend:   Community Attrition Management")
         
         "---"
-        #ajouter data 
+    
         df = pd.read_csv('token_data.csv')
         df.date_premiere_cap=pd.to_datetime(df.date_premiere_cap,format='%Y/%m/%d').dt.date
         
-        # --- NAVIGATION MENU ---
         selected = option_menu(
         menu_title=None,
         options=["Data", "Modeling"],
-        icons=["bar-chart-fill", "bi-graph-up-arrow"],  # https://icons.getbootstrap.com/
+        icons=["bar-chart-fill", "bi-graph-up-arrow"],  
         orientation="horizontal",
         )
        
        
         if selected == "Data":
             info1,info2=st.columns(2)
-             # Graph (Pie Chart )
+        
              
             df2= pd.read_csv('target.csv')
             df_target = df2[['token', 'profil_utilisateur']].groupby('profil_utilisateur').count() / len(df2)
             colours = ['#9ed5d6','#24abb0']
-            #contour='line':dict(color='#D33682', width=)
             fig_target = go.Figure(data=[go.Pie(labels=["Active","Inactive"],
                                                 values=df_target['token']
                                             ,marker= {'colors': colours,'line':dict(color='#D33682', width=0.7)},hole=.5)])
@@ -167,8 +138,6 @@ if authentication_status:
                 if show_data   :
                     st.dataframe(df.head())
                 
-                # Display statistical information on the dataset.
-                #st.subheader('Statistical Info about the Data')
                 stat = st.checkbox('Click to show Statistical Info ')
                 if stat:
                     st.write(df.describe())
@@ -176,7 +145,6 @@ if authentication_status:
             st.info('Active users: They are people who have less than 5 months of inactivity', icon="ℹ️")
 
             "---"
-        #  La distribution des utilisateurs inactifs
             st.subheader("Distribution of inactive users")
             
             viz_year, comparatif_viz = st.tabs(['Choose a year', 'Compare the years'])
@@ -190,8 +158,6 @@ if authentication_status:
                 inform = f"The chart for {contact_selected} :"
                 fig = px.line(df[df["year"]==contact_selected], x="date_premiere_cap", y="profil_utilisateur", title=inform)
 
-                #st.plotly_chart(fig, use_container_width=True)
-                # Plot 
                 fig2 = px.line(df_filter, x='date_premiere_cap', y='profil_utilisateur',
                         
                             )
@@ -221,23 +187,17 @@ if authentication_status:
             
         if selected=="Modeling":
         
-        
-        #Partie de prediction 
-        # Selection for a specific time frame.
             st.subheader('Model')
             df_select = df 
 
             col1, col2 = st.columns(2)
             "---"
             with col1:
-                #st.write('Select a Start Date to predict ')
                 start_date = st.date_input('Start Date',min_value= datetime.date(2021,9,27),max_value=datetime.date(2022,11,29),value=datetime.date(2021,9,27))
 
             with col2:    
-                #st.write('Select an End Date to predict ')
                 end_date = st.date_input('End Date',min_value=datetime.date(2021,9,27),max_value=datetime.date(2023,1,31),value=datetime.date(2023,1,31))
             
-            #Model
         
             if st.checkbox('Period Selected to predict'):
                 if(start_date != None or end_date != None):
@@ -258,14 +218,10 @@ if authentication_status:
                         model = SARIMAX(y_train,X_train, order=(3, 0, 0))
                         model.fit()
                     
-                        # Prédisez les données de test en utilisant le modèle entraîné
                         predictions = model.fit().predict(start=len(y_train), end=len(y_train)+len(y_test)-1, exog=X_test)
-                        # #inverse_transform train
                         forecast_test = scaler.inverse_transform(predictions.reshape(-1, 1)).reshape(-1, )
                         test["Forecast_ARIMAX"] = forecast_test
-                        #st.line_chart(test[['profil_utilisateur',"Forecast_ARIMAX"]],width=0, height=0, use_container_width=True)
                         
-                    #   # create a Plotly line plot
                         line_fig = px.line(
                         test.reset_index(),
                         x="date_premiere_cap",
@@ -274,12 +230,10 @@ if authentication_status:
                         labels={
                             "date_premiere_cap": " Date",
                             "profil_utilisateur": "Users",
-                            #"variable": "Legend"
+                            
                         }
                         )
 
-                        # Plotly graph configs
-                        #legend_names = {"sales": "Actual Sales", "sales_forecast": "Forecasted Sales"}
                 
                         line_fig.update_layout(
                         xaxis=dict(showgrid=True),
@@ -293,7 +247,6 @@ if authentication_status:
                         height=600
                         )
 
-                        # passing in the Plotly graph object to Streamlit
                         st.plotly_chart(line_fig, use_container_width=True) 
                 
                     else:
@@ -301,32 +254,25 @@ if authentication_status:
                 "---"
                 if st.button('Predict'):
                     r2=format(round(r2_score(test["profil_utilisateur"],test["Forecast_ARIMAX"])*100,2),",")
-                    #st.metric(value=r2,label="% of precesion")
                     actual_volume = format(test["profil_utilisateur"].sum(), ",")
                     predicted_volume = format(round(test["Forecast_ARIMAX"].sum()), ",")
                     val_abs=round((abs(int(actual_volume)-int(predicted_volume))/int(actual_volume))*100,2)
                     metric_col1, metric_col2, metric_col3,metric_col4= st.columns(4)
                     
                     personnes="Users"
-                    #metric_col1.markdown('##### Actual Volume:\n')
                     metric_col1.metric('Actual Volume',f"{actual_volume} {personnes}")
 
         
-                    #metric_col2.markdown('##### Forecasted Volume:\n')
                     metric_col2.metric('Forecasted Volume',f"{predicted_volume}"+" Users")
                     
-                    #metric_col3.markdown('##### R2:\n')
                     metric_col3.metric('R2',f"{r2}"+"%")
                     
-                    #metric_col4.markdown('##### Error:\n')
                     metric_col4.metric('Error',f"{val_abs}"+"%")
                 
 
-                    # line break to separate metrics from plot
                     """---"""
                     
-                    #st.write(r2,'% of Precision ')
-                    #st.warning('This is a warning', icon="⚠️")
+                   
                     
                 
                 
