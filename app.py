4af12452b5acc86190e82ce9e72281f787d3df95
amani <ellafi.amani@gmail.com>
2023-01-20 11:05:52 +0100

    authen2

--- app.py
+++ app.py
@@ -30,36 +30,43 @@ def load_lottiefile(filepath: str):
 
 st.set_page_config(page_title="Community Attrition Management", page_icon="::chart_with_upwards_trend:", layout="wide")
 def check_password():
-    """Returns `True` if the user had the correct password."""
+    """Returns `True` if the user had a correct password."""
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
+        if (
+            st.session_state["username"] in st.secrets["passwords"]
+            and st.session_state["password"]
+            == st.secrets["passwords"][st.session_state["username"]]
+        ):
             st.session_state["password_correct"] = True
-            del st.session_state["password"]  # don't store password
+            del st.session_state["password"]  # don't store username + password
+            del st.session_state["username"]
         else:
             st.session_state["password_correct"] = False
 
     if "password_correct" not in st.session_state:
-        # First run, show input for password.
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
         return False
     elif not st.session_state["password_correct"]:
         # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
         st.text_input(
             "Password", type="password", on_change=password_entered, key="password"
         )
-        st.error("😕 Password incorrect")
+        st.error("😕 User not known or password incorrect")
         return False
     else:
         # Password correct.
         return True
 
 if check_password():
-    st.write("Here goes your normal Streamlit app...")
-    st.button("Click me")
+    st.sidebar.write("Here goes your normal Streamlit app...")
+    st.sidebar.button("Click me")
 
 # users=db.fetch_all_users() 
 # usernames=[user["key"] for user in users]
